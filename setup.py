0e837e1d21574f230e2e111084e4276f484f47c6
FrischWaugh <philippe.jacques2137@gmail.com>
2019-10-30 21:31:28 -0400

    This is the first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict the next winner of the stanley cup.',
+    author='PJ-ATL',
+    license='MIT',
+)
